576d2a721b77096eeb74f757263e6290d569f645
owen656 <owen.j.agnello@gmail.com>
2026-01-31 12:55:20 -0800

    QUIT and custom errors -deleted main.py-

--- Physics.py
+++ Physics.py
@@ -1,5 +1,16 @@
 import pygame
 import Object
 
-def run():
-    Object.add_object()
+screen = pygame.display.set_mode((600,600))
+
+class PhysicsObjectError(Exception):
+     def __init__(self, message:str):
+        super().__init__(message)
+        self.message = message
+
+running = True
+Object.add_object()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            raise PhysicsObjectError("User tried to QUIT")
--- main.py
+++
@@ -1,3 +0,0 @@
-import Physics
-
-Physics.run()
