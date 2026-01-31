2b215042b7b95834c63510ca46694bea1a06f59f
owen656 <owen.j.agnello@gmail.com>
2026-01-31 13:17:33 -0800

    Did not Fix xspeed2 bugs, remeber to fix and added gravity

--- Object.py
+++ Object.py
@@ -4,19 +4,44 @@ if __name__ == "__main__":
 
 import pygame
 
+gravity = 0.8
+friction = 0.8
+
 class Sphere(pygame.sprite.Sprite):
     def __init__(self,position,radius,xspeed,yspeed):
         super().__init__()
         self.x,self.y  = position
         self.radius = radius
-        self.xspeed = xspeed
-        self.yspeed = yspeed
+        self.xspeed2 = xspeed
+        self.yspeed2 = yspeed
         self.image = pygame.Surface((self.x,self.y), pygame.SRCALPHA)
-    
-    def update(self):
-        pass
-        
+        self.rect = self.image.get_rect()
+        self.screen = pygame.display.get_surface()
+        self.screen_length = self.screen.get_width()
+        self.screen_height = self.screen.get_height()
+
+    def update(self,screen):
+        self.x += self.xspeed2
+        self.y += self.yspeed2
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        self.yspeed2 += gravity
+
+        if self.rect.bottom > self.screen_height:
+            self.yspeed2 = -self.yspeed2
+        if self.rect.top < 0:
+            self.yspeed2 = -self.yspeed2
+        if self.rect.left < 0:
+            self.xspeed2 = -self.xspeed2
+        if self.rect.left > self.screen_length:
+            self.xspeed2 = -self.xspeed2
+        pygame.draw.circle(screen, (255,0,0), (int(self.x),int(self.y)), self.radius)
+
 objects = pygame.sprite.Group()
 
 def add_object():
-    objects.add(Sphere((0,0),50,0,0))
+    objects.add(Sphere((200,200),50,0,0))
+
+def update(screen):
+    objects.update(screen)
--- Physics.py
+++ Physics.py
@@ -2,6 +2,7 @@ import pygame
 import Object
 
 screen = pygame.display.set_mode((600,600))
+clock = pygame.time.Clock()
 
 class PhysicsObjectError(Exception):
      def __init__(self, message:str):
@@ -9,8 +10,16 @@ class PhysicsObjectError(Exception):
         self.message = message
 
 running = True
+
 Object.add_object()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             raise PhysicsObjectError("User tried to QUIT")
+    
+
+    screen.fill((255,255,255))
+    Object.update(screen)
+
+    clock.tick(90)
+    pygame.display.flip()
